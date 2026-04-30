028226bf9b95dc2642d8d1291566df7bd8d3dcba
Johan Vela Arevalo <johan.vela@unas.edu.pe>
2026-04-30 09:45:31 -0500

    Integración sesión 6

---
+++ src/main/java/pe/unas/demoapi/domain/Producto.java
@@ -0,0 +1,13 @@
+package pe.unas.demoapi.domain;
+
+public class Producto {
+    private String nombre;
+    public Producto(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}
+
+
